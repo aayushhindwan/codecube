b6919c8afea40743567400dfb5816f712967597e
Aayush Hindwan <53999933+aayushhindwan@users.noreply.github.com>
2020-07-29 00:25:59 +0530

    Add files via upload

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import '../assests/scss/home.scss'
 import {Button} from 'react-bootstrap'
+import Doubt_Question from './doubt_question.js'
 
 export default class home extends Component {
     static propTypes = {
@@ -12,32 +13,10 @@ export default class home extends Component {
         return (
             <div className="main_div">
                    <div className="announcement">
+                        <Doubt_Question/> 
                          
                    </div>
-                   <div className="posts">
-                       <div className="authorId">
-                            <img src="https://homepages.cae.wisc.edu/~ece533/images/goldhill.png" alt="no-img" />
-                            <div className="userName"> 
-                                <div>shivam singh</div>
-                                <div className="postedOn">postedOn. 19 July</div>
-                            </div>
-                        </div>                            
-                        <div className="postQuestions"> 
-                        This is my Questions are the c languages that is used for reference pointer
-                        This is my Questions are the c languages that is used for reference pointer
-                        </div> 
-                        <div className="answerPost">
-                        This is my Questions are the c languages that is used for reference pointer
-                        This is my Questions are the c languages that is used for reference pointer
-                        This is my Questions are the c languages that is used for reference pointer
-                        This is my Questions are the c languages that is used for reference pointer
-                        This is my Questions are the c languages that is used for reference pointer
-                        This is my Questions are the c languages that is used for reference pointer......
-                        </div>
-                        <div className="answerBtn">
-                            <Button> Answer </Button>
-                        </div>
-                   </div>
+                  
             </div>
         )
     }
