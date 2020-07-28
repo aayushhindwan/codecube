2b32622fd86ed72ab6f303ca7ca95209efc368af
Aayush Hindwan <53999933+aayushhindwan@users.noreply.github.com>
2020-07-29 00:25:26 +0530

    Add files via upload

---
+++ frontend/src/components/doubt_question.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {Button} from 'react-bootstrap'
+
+export default class Doubt_Question extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    render() {
+        return (
+          <div className="posts">
+                       <div className="authorId">
+                            <img src="https://homepages.cae.wisc.edu/~ece533/images/goldhill.png" alt="no-img" />
+                            <div className="userName"> 
+                                <div>shivam singh</div>
+                                <div className="postedOn">postedOn. 19 July</div>
+                            </div>
+                        </div>                            
+                        <div className="postQuestions"> 
+                        This is my Questions are the c languages that is used for reference pointer
+                        This is my Questions are the c languages that is used for reference pointer
+                        </div> 
+                        <div className="answerPost">
+                        This is my Questions are the c languages that is used for reference pointer
+                        This is my Questions are the c languages that is used for reference pointer
+                        This is my Questions are the c languages that is used for reference pointer
+                        This is my Questions are the c languages that is used for reference pointer
+                        This is my Questions are the c languages that is used for reference pointer
+                        This is my Questions are the c languages that is used for reference pointer......
+                        </div>
+                        <div className="answerBtn">
+                            <Button> Answer </Button>
+                        </div>
+                   </div>
+        )
+    }
+}
