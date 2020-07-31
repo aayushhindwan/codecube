d76e10eef8a537ab13a61d464f0de17653c897dd
shivamsinghking <singhsanajay20001@gmail.com>
2020-08-01 01:02:37 +0530

    user_profile

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -1,11 +1,31 @@
-import React from 'react'
-
-
+import React, { Component } from 'react'
+import {Button} from 'react-bootstrap'
+import '../assests/scss/profile.scss'
 
 function Profile(){
+        let Component;
+   function showComponent(item){
+            //  if(item == "Doubts"){return Component = <Doubts />}
+            //  if(item == "Posts"){return Component = <Posts />}
+            //  if(item == "Articles"){return Component = <Articles />}
+
+    }
     return (
-        <div>
-            profile
+        <div class="main_profile_div">
+          <div className="upper_div">
+              <div  className="userProfile"> 
+               <div className="profile_pic"><img src ="https://homepages.cae.wisc.edu/~ece533/images/mountain.png" alt="no_image"/></div> 
+                <div><h2>shivam sigh</h2></div>
+                </div>
+          </div>
+          <div className="scrollbar">
+               <div onClick={()=> showComponent("Doubts")}>My Doubts</div>
+               <div>My Articles</div>
+               <div>My Posts</div>
+          </div>
+          <div className="Doubts_div">
+           {Component}
+          </div>
         </div>
     )
 }
