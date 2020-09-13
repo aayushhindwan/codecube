134fece2409048a16a3374a4caed9b85af8b8dfe
shivamsinghking <singhsanajay20001@gmail.com>
2020-09-13 10:01:05 +0530

    profile

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -29,8 +29,41 @@ function MyVerticallyCenteredModal(props) {
   );
 }
 
+
+function MyVerticallyCenteredModalProfile(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Change Profile
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+       <form className="profile-changes" method="POST">
+         <h1>Username</h1>
+        <input type = "text" /> <br /> 
+       </form> 
+       
+      </Modal.Body>
+      <Modal.Footer>
+      <Button type="submit"
+        onClick={props.onHide}
+      >Changes</Button>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 function Profile(){
   const [modalShow, setModalShow] = React.useState(false);
+  const [modalShowProfile, setModalShowProfile] = React.useState(false);
+
         let Component;
    function showComponent(item){
             //  if(item == "Doubts"){return Component = <Doubts />}
@@ -45,11 +78,20 @@ function Profile(){
       show={modalShow}
       onHide={() => setModalShow(false)}
     />
+     <MyVerticallyCenteredModalProfile
+      show={modalShowProfile}
+      onHide={() => setModalShowProfile(false)}
+    />
         <div class="main_profile_div">
           <div className="upper_div">
               <div  className="userProfile"> 
                <div className="profile_pic"><img src ="https://homepages.cae.wisc.edu/~ece533/images/mountain.png" alt="no_image"/></div> 
-                <div><h2>shivam sigh</h2></div>
+                <div><h2>shivam sigh</h2>
+                <Button
+                onClick={() => setModalShowProfile(true)}
+                >Changes Profile</Button>
+                </div>
+               
                </div>
               <div className="addSkills">
                 <Button onClick={() => setModalShow(true)}>Add Your Skills</Button>
