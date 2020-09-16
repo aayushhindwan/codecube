0b52ad7f68537b670117774ce93489486c08b071
shivamsinghking <singhsanajay20001@gmail.com>
2020-09-16 19:59:12 +0530

    tags

--- frontend/src/components/doubt_post.js
+++ frontend/src/components/doubt_post.js
@@ -41,6 +41,84 @@ handleTitleChange=(event)=> {
     this.setState({tags: event.target.value});
 
   }
+  componentDidMount(){
+    [].forEach.call(document.getElementsByClassName('tags-input'), function (el) {
+      let hiddenInput = document.createElement('input'),
+          mainInput = document.createElement('input'),
+          tags = [];
+      
+      hiddenInput.setAttribute('type', 'hidden');
+      hiddenInput.setAttribute('name', el.getAttribute('data-name'));
+  
+      mainInput.setAttribute('type', 'text');
+      mainInput.classList.add('main-input');
+      mainInput.addEventListener('input', function () {
+          let enteredTags = mainInput.value.split(' ');
+          if (enteredTags.length > 1) {
+              enteredTags.forEach(function (t) {
+                  let filteredTag = filterTag(t);
+                  if (filteredTag.length > 0)
+                      addTag(filteredTag);
+              });
+              mainInput.value = '';
+          }
+      });
+  
+      mainInput.addEventListener('keydown', function (e) {
+          let keyCode = e.which || e.keyCode;
+          if (keyCode === 8 && mainInput.value.length === 0 && tags.length > 0) {
+              removeTag(tags.length - 1);
+          }
+      });
+  
+      el.appendChild(mainInput);
+      el.appendChild(hiddenInput);
+  
+      addTag('hello!');
+  
+      function addTag (text) {
+          let tag = {
+              text: text,
+              element: document.createElement('span'),
+          };
+  
+          tag.element.classList.add('tag');
+          tag.element.textContent = tag.text;
+  
+          let closeBtn = document.createElement('span');
+          closeBtn.classList.add('close');
+          closeBtn.addEventListener('click', function () {
+              removeTag(tags.indexOf(tag));
+          });
+          tag.element.appendChild(closeBtn);
+  
+          tags.push(tag);
+  
+          el.insertBefore(tag.element, mainInput);
+  
+          refreshTags();
+      }
+  
+      function removeTag (index) {
+          let tag = tags[index];
+          tags.splice(index, 1);
+          el.removeChild(tag.element);
+          refreshTags();
+      }
+  
+      function refreshTags () {
+          let tagsList = [];
+          tags.forEach(function (t) {
+              tagsList.push(t.text);
+          });
+          hiddenInput.value = tagsList.join(',');
+      }
+  
+      function filterTag (tag) {
+          return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
+      }
+  });
+  }
 render()
 {
   return (
@@ -57,7 +135,10 @@ render()
           }}
          />
          <legend>Tags</legend>
-    <input type="text" placeholder="tags of your doubts" onChange={this.handleTagChange} value={this.state.tags} />
+    {/* <input type="text" placeholder="tags of your doubts" onChange={this.handleTagChange} value={this.state.tags} /> */}
+         <div className="tags-input" data-name="tags-input">
+                {/* <span class="tag">HTML<span class="close"></span></span> */}
+            </div>
     <br/>
     <Button onClick={this.SubmitDoubt} >Submit Doubt</Button>
     </div>     
